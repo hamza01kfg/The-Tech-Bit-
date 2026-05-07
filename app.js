ae87f89de0ae049333c8c37b9d6906182d21ee6d
hamza01kfg <abdulwaheed050301@gmail.com>
2026-05-07 06:19:13 +0500

    Update app.js

--- app.js
+++ app.js
@@ -499,7 +499,7 @@ document.addEventListener('DOMContentLoaded', () => {
 // Service Worker registration
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/sw.js').then(reg => {
+        navigator.serviceWorker.register('/The-Tech-Bit-/sw.js').then(reg => {
             console.log('Service Worker registered:', reg.scope);
         }).catch(err => {
             console.log('Service Worker registration failed:', err);
